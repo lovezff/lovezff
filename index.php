8a85dd6f217bcc0400a002dea5c6b45483d1efea
lovezff <976589577@qq.com>
2016-05-20 21:28:47 +0800

    love2

---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE HTML>
+<!--[if IE 6]>
+<html id="ie6" class="ie ie6 lt-ie9">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" class="ie ie7 lt-ie9">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" class="ie ie8 lt-ie9">
+<![endif]-->
+<!--[if gte IE 9]>
+<html class="ie ie9">
+<![endif]-->
+<!--[if !(IE)]><!-->
+<html>
+<!--<![endif]-->
+<head>
+	<meta http-equiv='Pragma' content='no-cache' />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta name="author" content="情人节,表白程序,情人节表白程序,自动生成表白程序,自动表白程序">
+	
+	<title>情人节自动生成表白程序</title>
+	<link rel="stylesheet" href="css/all.min.css">
+</head>
+<body>
+	<div class="flowtime">
+		<div class="ft-section section-1" data-id="section-1">
+			<div id="/section-1/page-1" class="ft-page page-1" data-id="page-1">
+				<p class="text1"><span id="text-75" contenteditable="true">兵哥哥</span> ❤ <span id="text-76" contenteditable="true">小芳芳</span> <span id="text-77" contenteditable="true">说好不分开</span></p>
+				<p class="text2">按键盘 "↓" <span id="text-78" contenteditable="true">开始倾听我的内心</span></p>
+			</div>
+			<div id="/section-1/page-2" class="ft-page page-2" data-id="page-2">
+				<p class="top-text" id="text-1" contenteditable="true">刚踏入大学的小芳芳对爱情充满了期待。</p>
+				<img src='img/iali63.jpg'  alt="刚踏入大学的小芳芳对爱情充满了期待。" />
+			</div>
+			<div id="/section-1/page-3" class="ft-page page-3 left-img" data-id="page-3">
+				<h2 class="text" id="text-2" contenteditable="true">自从遇到了兵哥哥</h2>
+				<img src='img/iali35.jpg'  alt="自从遇到了兵哥哥"/>
+			</div>
+			<div id="/section-1/page-4" class="ft-page page-4 " data-id="page-4">
+				<h2 class="center-text" id="text-3" contenteditable="true">我们的生活变得精彩</h2>
+				<img src='img/iali6.jpg'  alt="我们的生活变得精彩" />
+			</div>
+		</div>
+		<div class="ft-section section-2" data-id="section-2">
+			<div id="/section-2/page-1" class="ft-page page-5 " data-id="page-1">
+				<h2 class="center-text" id="text-4" contenteditable="true">我们热恋</h2>
+				<img src='img/iali19.jpg'  alt="我们热恋" />
+			</div>
+			<div id="/section-2/page-2" class="ft-page page-3 "  data-id="page-2">
+				<h2 class="center-text" id="text-5" contenteditable="true">生命中再也离不开彼此</h2>
+				<img src='img/iali29.jpg' alt="生命中再也离不开彼此"/>
+			</div>
+			<div id="/section-2/page-3" class="ft-page page-7 left-img" data-id="page-3">
+				<h3 id="text-6" contenteditable="true">小芳芳，我们回忆过去的美好，希望更加坚强地走下去！</h3>
+				<img src='img/iali60.jpg'  alt="小芳芳，我们回忆过去的美好，希望更加坚强地走下去！"/>
+			</div>
+			<div id="/section-2/page-4" class="ft-page page-8 right-img " data-id="page-4">
+				<h3 id="text-7" contenteditable="true">我们的经历了最美好的时光</h3>
+				<img src='img/iali51.jpg' alt="我们的经历了最美好的时光" />
+			</div>
+			<div id="/section-2/page-5" class="ft-page page-9 right-img" data-id="page-5">
+				<h3 id="text-8" contenteditable="true">将来想带你去任何地方</h3>
+				<img src='img/iali5.jpg' />
+			</div>
+			<div id="/section-2/page-6" class="ft-page page-10" data-id="page-6">
+				<h3 id="text-9" contenteditable="true">有这么可爱萌萌哒的老婆，是我一生的荣幸</h3>
+				<img src='img/iali22.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-3" data-id="section-3">
+			<div id="/section-3/page-1" class="ft-page page-11 " data-id="page-1">
+				<h3 id="text-10" contenteditable="true">我有时也那么2B,吃冰淇淋都那么陶醉</h3>
+				<img src='img/iali24.jpg'  />
+			</div>
+			<div id="/section-3/page-2" class="ft-page page-12 left-img" data-id="page-2">
+				<h3><span id="text-11" contenteditable="true">回忆起我们在一起的时光</span><br /><span id="text-12" contenteditable="true">还是那么的美好</span></h3>
+				<img src='img/iali25.jpg' />
+			</div>
+			<div id="/section-3/page-3" class="ft-page page-13" data-id="page-3">
+				<img src='img/iali64.jpg' alt='我发誓这辈子一定要娶你！' />
+				<h3 id="text-13" contenteditable="true">我发誓这辈子一定要娶你！</h3>
+			</div>
+			<div id="/section-3/page-4" class="ft-page page-14 left-img" data-id="page-4">
+				<h3 id="text-14" contenteditable="true">发完誓继续吃冰淇淋... ...</h3>
+				<img src='img/iali46.jpg' />
+			</div>
+			<div id="/section-3/page-5" class="ft-page page-15 left-img" data-id="page-5">
+				<h3><span id="text-15" contenteditable="true">当然一定符合你对美好的一切想象</span><br /><span id="text-16" contenteditable="true">给你梦中的婚礼，在某一个地方</span></h3>
+				<img src='img/iali45.jpg' />
+			</div>
+			<div id="/section-3/page-6" class="ft-page page-16 left-img" data-id="page-6">
+				<h3><span id="text-17" contenteditable="true">再看看咱们的合照</span><br /><span id="text-18" contenteditable="true">在最繁华的商店里</span></h3>
+				<img src='img/iali66.jpg' />
+			</div>
+			<div id="/section-3/page-7" class="ft-page page-17 top-text" data-id="page-7">
+				<h3 id="text-19" contenteditable="true">小兔子，在我心中你就是小兔子...啊不...一样纯洁</h3>
+				<img src='img/iali16.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-4" data-id="section-4">
+			<div id="/section-4/page-1" class="ft-page page-18 full-img" data-id="page-1">
+				<p class="text" id="text-20" contenteditable="true">还记得咱们大晚上去的甜爱路，打着手电给你念墙上的情诗</p>
+				<img src='img/iali11.jpg'  />
+			</div>
+			<div id="/section-4/page-2" class="ft-page page-19" data-id="page-2">
+				<h3 id="text-21" contenteditable="true">给你买的口罩耳暖，陪你度过一个冬天，真的很可爱</h3>
+				<img src="img/iali75.jpg" />
+			</div>
+			<div id="/section-4/page-3" class="ft-page page-20 left-img" data-id="page-3">
+				<img src='img/iali59.png' />
+				<h3 id="text-22" contenteditable="true">咱们一起做的一顿饭</h3>
+				<img src='img/iali9.jpg' />
+			</div>
+			<div id="/section-4/page-4" class="ft-page page-21" data-id="page-4">
+				<h3 id="text-23" contenteditable="true">被你捉弄的发型</h3>
+				<img src='img/iali59.png' />
+			</div>
+			<div id="/section-4/page-5" class="ft-page page-22 left-img" data-id="page-5">
+				<h3 id="text-24" contenteditable="true">现在很少出去玩了</h3>
+				<img src='img/iali4.jpg' />
+			</div>
+			<div id="/section-4/page-6" class="ft-page page-23 left-img" data-id="page-6">
+				<h3 id="text-25" contenteditable="true">也没时间去胡吃海喝了</h3>
+				<img src='img/iali44.jpg' />
+			</div>
+			<div id="/section-4/page-7" class="ft-page page-24 right-img" data-id="page-7">	
+				<h3 id="text-26" contenteditable="true">我眼里只有你</h3>
+				<img src='img/iali32.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-5" data-id="section-5">
+			<div id="/section-5/page-1" class="ft-page page-25 left-img" data-id="page-1">
+				<h3><span id="text-27" contenteditable="true">黑白照片</span><br /><span id="text-28" contenteditable="true">不要乱想</span></h3>
+				<img src='img/iali37.jpg' />
+			</div>
+			<div id="/section-5/page-2" class="ft-page page-26 top-text" data-id="page-2">
+				<h3 id="text-29" contenteditable="true">当时最喜欢当壁纸的一张照片</h3>
+				<img src='img/iali10.gif' />
+			</div>
+			<div id="/section-5/page-3" class="ft-page page-27" data-id="page-3">
+				<h3 id="text-30" contenteditable="true">第一份工作，迫不得已没法陪在你身边</h3>
+				<img src='img/iali67.jpg'  />
+			</div>
+			<div id="/section-5/page-4" class="ft-page page-28" data-id="page-4">
+				<h3 id="text-31" contenteditable="true">好怀念一起在西门的小餐馆吃饭</h3>
+				<img src='img/iali40.jpg' />
+			</div>
+			<div id="/section-5/page-5" class="ft-page page-29" data-id="page-5">
+				<h3 id="text-32" contenteditable="true">萌萌哒小学妹</h3>
+				<img src='img/iali12.jpg' />
+			</div>
+			<div id="/section-5/page-6" class="ft-page page-30 left-img" data-id="page-6">
+				<h3 id="text-33" contenteditable="true">咱们买的不配套的情侣装</h3>
+				<img src='img/iali76.gif' />
+			</div>
+			<div id="/section-5/page-7" class="ft-page page-31" data-id="page-7">
+				<h3 id="text-34" contenteditable="true">彩虹桥兜风</h3>
+				<img src='img/iali77.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-6" data-id="section-6">
+			<div id="/section-6/page-1" class="ft-page page-32" data-id="page-1">
+				<h3 id="text-35" contenteditable="true">咱们都没有艺术细胞，去艺术馆欣赏不了</h3>
+				<img src='img/iali62.jpg'  />
+			</div>
+			<div id="/section-6/page-2" class="ft-page page-33 top-text" data-id="page-2">
+				<h3 id="text-36" contenteditable="true">不知道什么时候在餐厅吃饭，偷拍了一张背影</h3>
+				<img src='img/iali3.jpg' />
+			</div>
+			<div id="/section-6/page-3" class="ft-page page-34 " data-id="page-3">
+				<h3 id="text-37" contenteditable="true">后边小人贱贱的</h3>
+				<img src='img/iali57_bg.jpg' />
+			</div>
+			<div id="/section-6/page-4" class="ft-page page-35 " data-id="page-4">
+				<h3><span id="text-38" contenteditable="true">有你真幸福</h3>
+				<img src='img/iali8.gif' />
+			</div>
+			<div id="/section-6/page-5" class="ft-page page-36 " data-id="page-5">
+				<h3 id="text-41" contenteditable="true">自我感觉良好</h3>
+				<img src='img/iali31.jpg' />
+			</div>
+			<div id="/section-6/page-6" class="ft-page page-37 left-img" data-id="page-6">
+				<h3 id="text-42" contenteditable="true">傻傻的小芳芳</h3>
+				<img src='img/iali49.jpg' />
+			</div>
+			<div id="/section-6/page-7" class="ft-page page-38 bottom-text" data-id="page-7">
+				<img src='img/iali21.jpg' />
+				<h3 id="text-43" contenteditable="true">让人心疼的姑娘</h3>
+			</div>
+		</div>
+		<div class="ft-section section-7" data-id="section-7">
+			<div id="/section-7/page-1" class="ft-page page-39 left-img" data-id="page-1">
+				<h3 id="text-44" contenteditable="true">这可是你亲手做的花，现在我保存完好</h3>
+				<img src='img/iali2.jpg' />
+			</div>
+			<div id="/section-7/page-2" class="ft-page page-40" data-id="page-2">
+				<h3 id="text-45" contenteditable="true">让我紧紧的抱着你，不放开</h3>
+				<img src='img/iali14.jpg'  />
+			</div>
+			<div id="/section-7/page-3" class="ft-page page-41" data-id="page-3">
+				<img src='img/iali30_1.jpg' class='img1'/>
+				<img src='img/iali30_2.jpg' class='img2'/>
+				<h3 id="text-46" contenteditable="true"> 头发没了，奋斗宣言还在</h3>
+			</div>
+			<div id="/section-7/page-4" class="ft-page page-42 " data-id="page-4">
+				<h3><span id="text-47" contenteditable="true">我编的辫子，真好看</h3>
+				<img src='img/iali69.jpg'  />
+			</div>
+			<div id="/section-7/page-5" class="ft-page page-43" data-id="page-5">
+				<img src='img/iali42.jpg' />
+				<h3 id="text-49" contenteditable="true">我是乌龟王八蛋，不疼媳妇儿就该打</h3>
+			</div>
+			<div id="/section-7/page-6" class="ft-page page-44 left-img" data-id="page-6">
+				<h3><span id="text-50" contenteditable="true">还是舍不得离开</span><br /><span id="text-51" contenteditable="true">离别之前比较激动</span></h3>
+				<img src='img/iali0.jpg' />
+			</div>
+			<div id="/section-7/page-7" class="ft-page page-45 left-img" data-id="page-7">
+				<h3><span id="text-52" contenteditable="true">键盘咱也跪过</span><br /><span id="text-53" contenteditable="true">知错就改还是好男人</span></h3>
+				<img src='img/iali18.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-8" data-id="section-8">
+			<div id="/section-8/page-1" class="ft-page page-46" data-id="page-1">
+				<img src='img/iali29' />
+				<h3>
+					<span id="text-54" contenteditable="true">今天是520</span>
+					<span id="text-55" contenteditable="true">我要对你说：我爱你</span>
+					
+				</h3>
+			</div>
+			<div id="/section-8/page-2" class="ft-page page-47 top-text" data-id="page-2">
+				<img src='img/iali58.jpg' />
+				<h3>
+					<span id="text-58" contenteditable="true">想想后面还有一辈子</span>
+					<span id="text-59" contenteditable="true">希望我们一起走下去</span>
+					<span id="text-60" contenteditable="true">因为可以一直照顾小芳芳</span>
+					<span id="text-61" contenteditable="true">给你更多的温暖</span></h3>
+				
+			</div>
+			<div id="/section-8/page-3" class="ft-page page-48 left-img" data-id="page-3">
+				<p><span id="text-62" contenteditable="true">小芳芳不要苦恼，不要害怕</p>
+				<img src='img/iali65.jpg'  />
+			</div>
+			<div id="/section-8/page-4" class="ft-page page-49 right-img " data-id="page-4">
+			<img src='img/iali71.jpg' />
+				<p class="text">
+					<span class='text' id="text-64" contenteditable="true">我会一直陪在你身边 不离不弃</span>
+				</p>
+			</div>
+			<div id="/section-8/page-5" class="ft-page page-50" data-id="page-5">
+				<p class='text' id="text-66" contenteditable="true">就象这样你去哪我去哪</p>
+				<img src='img/iali50_1.jpg' class='img1' />
+				<img src='img/iali50_2.jpg' class='img2' />
+			</div>
+			<div id="/section-8/page-6" class="ft-page page-51" data-id="page-6">
+				<p>
+					<span id="text-67" contenteditable="true">你问：永远有多远？</span><br />
+					<span id="text-68" contenteditable="true">我说：比时间多一秒就是永远，我会永远爱你</span><br />
+					<span id="text-69" contenteditable="true">你再问：世界有多大？</span><br />
+					<span id="text-70" contenteditable="true">我说：你走到哪里，世界就有多大</span>
+					</p>
+			</div>
+		</div>
+		<div class="ft-section section-9" data-id="section-9">
+			<div id="/section-9/page-1" class="ft-page page-52 " data-id="page-1">
+				<h3 id="text-71" contenteditable="true">你离开我我会过得不开心</h3>
+				<img src='img/iali20.jpg'  />
+			</div>
+			<div id="/section-9/page-2" class="ft-page page-53 left-img" data-id="page-2">
+				<img src='img/iali68.jpg' alt='happy birthday' />
+				<h3 id="text-72" contenteditable="true">希望从现在开始你的心情好起来</h3>
+			</div>
+			<div id="/section-9/page-3" class="ft-page page-54 center-img" data-id="page-3" >
+				<div class="center-img">I love you</div>
+			</div>
+			<div id="/section-9/page-4" class="ft-page page-55 right-img" data-id="page-4">
+				<img src='img/14915.jpg' />
+				<p class="text">
+					<span id="text-74" contenteditable="true">就像这样我会一直等你</span><br />
+					<span id="text-73" contenteditable="true">希望咱们的故事会一直继续下去。</span><br />
+					<span id="text-74" contenteditable="true">无论精彩、平淡都会是我喜欢的喜欢的。</span><br />
+					
+					<br />
+					<br />
+					<span> -- 按“Esc"键有惊喜</span>
+				</p>				
+			</div>
+		</div>
+	</div>
+	<div class="nojavascript">您的浏览器版本太低或禁用了javascript，无法正常浏览本页面</div>
+	<div class="write-tip">
+		<span class="showtip">小提示：点击文字可以进行编辑，点击右下角心形可以跳跃浏览</span>
+		<span style="display:none">小提示：编辑完成后点击保存修改，发送给你的亲亲开始表白吧</span>
+		<button id="write-submit">保存修改</button>
+	</div>
+	<div class="write-ok">
+		<div class="write-box">
+			<h2>还差一步即可生成表白页面</h2>
+			<p id="write-mp3">自定义背景音乐：<span id="text-music" contenteditable="true">http://<?php echo $_SERVER['HTTP_HOST']; ?>/love/music/saveme</span><i></i>.mp3</p>
+			<p id="write-url">自定义表白链接：<u>http://<?php echo $_SERVER['HTTP_HOST']; ?>/love/2014/</u><span id="text-href" contenteditable="true"><?php echo time(); ?></span><i></i><u>.html</u></p>
+			<p><small>自定义链接名只能为3-30位的字母[a-zA-Z]、数字[0-9]、- 和 _ </small></p>
+			<small><a href="###" id="back">重新修改</a></small> <button id="write-post">❤ 生成表白页面</button>
+			<div class="write-share">
+				<p>~\(≧▽≦)/~ 点击分享 发送给你的亲亲吧</p>
+				<div class="bdsharebuttonbox"><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a><a href="#" class="bds_tqq" data-cmd="tqq" title="分享到腾讯微博"></a><a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网"></a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a><a href="#" class="bds_sqq" data-cmd="sqq" title="分享到QQ好友"></a><a href="#" class="bds_tieba" data-cmd="tieba" title="分享到百度贴吧"></a><a href="#" class="bds_copy" data-cmd="copy" title="分享到复制网址"></a><a href="#" class="bds_more" data-cmd="more"></a></div>
+			</div>
+		</div>
+	</div>
+
+	<audio id="bgmMusic" src="music/saveme.mp3" autoplay="autoplay" loop preload="auto" type="audio/mp3"></audio>
+    
+	<script src="http://libs.baidu.com/jquery/1.8.3/jquery.min.js"></script>
+	<script src="js/all.min.js"></script>
+	<script src="js/love.min.js"></script>
+    <div class="mPower"><span id="on" title="点击暂停"></span><span id="off" title="点击播放"></span></div>
+</body>
+</html>
